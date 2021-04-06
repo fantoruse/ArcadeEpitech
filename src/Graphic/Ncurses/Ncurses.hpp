f0fc4ef0203e36a3c6c792d2e0be3d678d633932
tcampos <theo.campos@epitech.eu>
2021-04-06 13:37:36 +0200

    edit: update the enum in ncurses.hpp

--- src/Graphic/Ncurses/Ncurses.hpp
+++ src/Graphic/Ncurses/Ncurses.hpp
@@ -19,7 +19,10 @@ static const std::vector<std::pair<int, arcade::events_e>> KEYS = {
 {KEY_DOWN, arcade::DOWN},
 {KEY_LEFT, arcade::LEFT},
 {KEY_RIGHT, arcade::RIGHT},
-{' ', arcade::SPACE},};
+{' ', arcade::SPACE},
+{'s', arcade::NEXT},
+{'q', arcade::PREV},
+};
 
 namespace arcade {
     class Ncurses : public arcade::ADisplayModule {
@@ -30,7 +33,7 @@ namespace arcade {
         void init() override;
         void destroy() override;
         void clearWin() override;
-       void refreshWin() override;
+        void refreshWin() override;
         void draw(std::vector<std::shared_ptr<IDrawable>> drawable, std::pair<int, int> position, std::string &name) override;
         events_e pollEvent() override;
         void load(std::vector<std::shared_ptr<IDrawable>> drawable, std::string &name) override;
